02da601a678dd47e9a6082a837f2863b92960e05
Dennis Dudek <dennis.dudek@adesso-mobile.de>
2024-03-23 19:56:26 +0100

    update

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,8 @@ import "reflect-metadata";
 import { TypeORM } from "$lib/server/database/db";
 import type {Handle} from "@sveltejs/kit";
 import {createNewSessionId, createOrGet, set} from "$lib/server/Session/SessionStorage";
-const db = await TypeORM.getInstance();
+const db = await TypeORM.getInstance(); //we init the db connection
+
 
 
 
