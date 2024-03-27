396d2fc3deef882f88497b9de33677228ea925e7
Dennis Dudek <dennis.dudek@adesso-mobile.de>
2024-03-27 11:37:02 +0100

    new injection

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { createWSSGlobalInstance, onHttpServerUpgrade } from './src/lib/server/WebSocketUtil.js';
 import fs from 'fs';
+
+let closing = false;
 export default defineConfig({
 	build: {
 		target: 'esnext'
@@ -23,18 +25,29 @@ export default defineConfig({
 		},
 		{
 			name: 'injectWebsocketServer',
-			buildEnd: async function(){
-				//ls -la ./buid
-				const fileList = fs.readdirSync('./');
-				console.log(fileList);
-				const fileList2 = fs.readdirSync('./build');
-				console.log(fileList2);
-				//rename ./build/index.js to ./build/node-index.js
-				fs.renameSync('./build/index.js', './build/node-index.js');
-				//copy ./src/lib/server/WebSocketUtil.js to ./build/WebSocketUtil.js
-				fs.copyFileSync('./src/lib/server/WebSocketUtil.js', './build/WebSocketUtil.js');
-				//copy ./prodserver.js to ./build/index.js
-				fs.copyFileSync('./prodserver.js', './build/index.js');
+			closeBundle: async function(){
+				process.once('exit', function() {
+
+					const prepath = './build/';
+					if(fs.existsSync(prepath+'/injected')) return;
+					fs.writeFileSync(prepath+'/injected', '');//create file to indicate that it has been injected
+					console.log('injecting websocket server');
+					let indexSrc = fs.readFileSync(prepath + 'index.js', 'utf8');
+					let prodserverSrc = fs.readFileSync('./prodserver.js', 'utf8');
+					let before = prodserverSrc.indexOf('//injecthere');
+					if(before !== -1) {
+
+						prodserverSrc = prodserverSrc.slice(0, before) + indexSrc + prodserverSrc.slice(before + 12);
+
+
+						fs.writeFileSync(prepath + 'index.js', prodserverSrc, 'utf8');
+
+
+						//copy ./src/lib/server/WebSocketUtil.js to ./build/WebSocketUtil.js
+						fs.copyFileSync('./src/lib/server/WebSocketUtil.js', prepath + 'WebSocketUtil.js');
+					}
+				});
+
 			}
 		}
 	]
