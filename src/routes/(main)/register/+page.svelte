c26a7ff0cadaa79c140a61c58da70c533dd7d0c4
Dennis Dudek <dennis.dudek@adesso-mobile.de>
2024-03-27 10:04:43 +0100

    smui not svelte5 ready

--- src/routes/(main)/register/+page.svelte
+++ src/routes/(main)/register/+page.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
-    import Select, { Option } from '@smui/select';
+
     import type { PageData } from './$types';
+    import {t} from "$lib/translations";
     let {data} : {data:PageData} = $props();
 </script>
-
+<svelte:head>
+    <title>{$t('website.title.register')}</title>
+</svelte:head>
 
 <div>
-    <Select>
-        {#each data.races as race}
-            <Option value={race}>{race}</Option>
-        {/each}
-    </Select>
+    <h3 class="text-xl mb-4">{$t('website.title.register')}</h3>
+    <div class="flex gap-4">
+        <label for="select-race">{$t('website.register.select-race')}</label>
+        <select id="select-race">
+            {#each data.races as race}
+                <option value={race}>{$t('race.' + race)}</option>
+            {/each}
+        </select>
+    </div>
+
 </div>
