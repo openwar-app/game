10586424dec5ca9ac133421b1fd9a7252cfd8292
Dennis Dudek <dennis.dudek@adesso-mobile.de>
2024-04-03 22:14:11 +0200

    remove some smells

--- src/routes/(main)/register/+server.ts
+++ src/routes/(main)/register/+server.ts
@@ -4,43 +4,47 @@ import {User} from "$lib/server/classes/User";
 import {validatePassword} from "$lib/shared/utils";
 export const POST: RequestHandler = async ({request}) => {
     const ctype = request.headers.get('content-type') ?? 'unknown';
-    if(ctype === 'application/json') {
-        const body = await request.json();
-        switch(body.action ?? '') {
-            case 'validatemail': {
-                const validate = await User.validateEmail(body.email, true);
-                return json(validate ? {status: 'ok' } : {status: 'error', error: validate});
-            }
-            case 'validatecharname': {
-                const validate = await User.validateCharname(body.charname, true);
-                return json(validate ? {status: 'ok'} : {status: 'error', error: validate});
-            }
+    if(ctype !== 'application/json') {
+        return json({}, { status: 400 });
+    }
 
-            case 'register': {
-                const validateEmail = await User.validateEmail(body.email, true);
-                if (validateEmail !== true) {
-                    return json({status: 'error', error: validateEmail});
-                }
-                const validateCharname = await User.validateCharname(body.charname, true);
-                if (validateCharname !== true) {
-                    return json({status: 'error', error: validateCharname});
-                }
-                const validatedPassword = validatePassword(body.password, body.password);
-                if(validatedPassword !== true) {
-                    return json(validatedPassword);
-                }
-                await User.create(
-                    {
-                        email: body.email,
-                        charname: body.charname,
-                        password: body.password,
-                        race: body.race
-                    }
-                );
-                return json({status: 'ok'});
+
+    const body = await request.json();
+    switch(body.action) {
+        case 'validatemail': {
+            const validate = await User.validateEmail(body.email, true);
+            return json(validate ? {status: 'ok' } : {status: 'error', error: validate});
+        }
+        case 'validatecharname': {
+            const validate = await User.validateCharname(body.charname, true);
+            return json(validate ? {status: 'ok'} : {status: 'error', error: validate});
+        }
+
+        case 'register': {
+            const validateEmail = await User.validateEmail(body.email, true);
+            if (validateEmail !== true) {
+                return json({status: 'error', error: validateEmail});
             }
+            const validateCharname = await User.validateCharname(body.charname, true);
+            if (validateCharname !== true) {
+                return json({status: 'error', error: validateCharname});
+            }
+            const validatedPassword = validatePassword(body.password, body.password);
+            if(validatedPassword !== true) {
+                return json(validatedPassword);
+            }
+            await User.create(
+                {
+                    email: body.email,
+                    charname: body.charname,
+                    password: body.password,
+                    race: body.race
+                }
+            );
+            return json({status: 'ok'});
         }
     }
+
 
 
 
