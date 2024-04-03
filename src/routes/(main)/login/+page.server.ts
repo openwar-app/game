a7e1a9db396adf1f01e888436b17b2f42bcd4105
Dennis Dudek <dennis.dudek@adesso-mobile.de>
2024-04-03 22:14:35 +0200

    remove locals log

--- src/routes/(main)/login/+page.server.ts
+++ src/routes/(main)/login/+page.server.ts
@@ -2,7 +2,6 @@ import type { PageServerLoad } from './$types';
 import Races from '$lib/shared/races';
 import {User} from "$lib/server/classes/User";
 export const load: PageServerLoad = async ({ locals }) => {
-    console.log(locals);
     let user = null;
     if(locals.session.data?.userid !== null) {
         let userObj = await User.byId(locals.session.data.userid);
